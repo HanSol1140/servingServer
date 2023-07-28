76ec63f13bf9a7443ec9d16099286aa179a00181
SongHanSol <skycriperd@gmail.com>
2023-07-28 19:08:24 +0900

    1

--- mappingtest.py
+++ mappingtest.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # 이미지를 로드합니다.
-image = cv2.imread('map.png', cv2.IMREAD_GRAYSCALE)  # 'your_image_file.png'을 실제 파일 경로로 바꿔주세요.
+image = cv2.imread('C:\\songhansol\\servingServer\\map.png', cv2.IMREAD_GRAYSCALE)  # 'your_image_file.png'을 실제 파일 경로로 바꿔주세요.
 
 # 클릭 이벤트를 처리하는 함수입니다.
 def on_click(event, x, y, flags, param):
