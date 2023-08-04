8e6e6b186198b7da67d648b527ac8b0a10dfb395
SongHanSol <skycriperd@gmail.com>
2023-08-04 19:06:02 +0900

    111

--- routerhandler.js
+++ routerhandler.js
@@ -9,7 +9,7 @@ router.get("/test1", async (req, res) => {
 
 router.get("/", async (req, res) => {
     try {
-        const response = await axios.get(`http://192.168.0.3/reeman/current_version`);
+        const response = await axios.get(`http://192.168.0.15/reeman/current_version`);
         if (await response.status === 200) {
             console.log(response.data);
         }
@@ -131,5 +131,55 @@ router.get("/api/getrobotlist", async (req, res) => {
         res.send(data); // 파일 데이터를 클라이언트에 응답으로 보냅니다.
     });
 });
+
+
+
+
+
+// ====================================================================================================
+// ====================================================================================================
+
+
+// 포인트저장
+router.post("/api/createPointList", async (req, res) => {
+    try {
+        // RobotSettings.json이 없다면 생성
+        if (!fs.existsSync('PointSettings.json')) {
+            fs.writeFileSync('PointSettings.json', JSON.stringify([]));
+        }
+        //파일 읽기
+        fs.readFile('PointSettings.json', 'utf8', (err, fileData) => {
+            if (err) throw err;
+            let data = [];
+            data = fileData ? JSON.parse(fileData) : []; // 파일에 데이터가 없으면 빈배열 생성
+            const newData = {
+                pointName : req.body.pointName,
+                coordinatesX : req.body.coordinatesX,
+                coordinatesY : req.body.coordinatesY,
+                coordinatesTheta : req.body.coordinatesTheta,
+            };
+            const exists1 = data.some(item => item.pointName === req.body.pointName);
+
+            if (!exists1) { // 포인트명에 중복이 없다.
+                
+                data.push(newData); // 새로운 데이터를 배열에 추가
+                const jsonData = JSON.stringify(data, null, 2);
+                fs.writeFileSync('PointSettings.json', jsonData); // 동기적으로 파일 작성
+                res.send("저장 완료");
+               
+            } else {
+                res.status(409).send("포인트의 명칭이 중복되었습니다. \n기입한 정보를 확인해주세요.");
+            }
+        });
+
+    } catch (error) {
+        console.error('Error with API call:', error);
+        console.log("error : ", error);
+    }
+});
+
+// ====================================================================================================
+// ====================================================================================================
+
 
 module.exports = router;
