8bdd3de310a211b559d40c1de8eadb9191c0f9a4
SongHanSol <skycriperd@gmail.com>
2023-08-30 18:59:33 +0900

    20230830

--- robotconfig.ts
+++ robotconfig.ts
@@ -6,13 +6,27 @@ export interface PointCoordinate {
     y?: string;
     theta?: string;
 }
-export let robotSettings: { [key: string]: {robotNumber:string, robotIP:string, robotRunningState:boolean, robotLastOrderPoint:PointCoordinate} } = {};
+export let robotSettings: {
+    [key: string]: {
+        robotNumber:string,
+        robotIP:string,
+        robotRunningState:boolean,
+        robotLastOrderPoint:PointCoordinate
+    }
+} = {};
 
 export function setRobotSettings(name: string, robotNumber:string, robotIP:string, robotRunningState:boolean, robotLastOrderPoint:PointCoordinate) {
     robotSettings[name] = { robotNumber, robotIP, robotRunningState, robotLastOrderPoint };
 }
+
 // pointCoordinate
-export let pointCoordinate: { [key: string]: { x: string, y: string, theta: string } } = {};
+export let pointCoordinate: {
+    [key: string]: {
+        x: string,
+        y: string,
+        theta: string
+    }
+} = {};
 
 export function setPointCoordinate(pointName: string, x: string, y: string, theta: string) {
     pointCoordinate[pointName] = { x, y, theta };
